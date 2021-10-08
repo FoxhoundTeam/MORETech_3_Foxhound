bfdf9eae6f75fc5975afd6dab29422a5fef10363
kirill_semennikov <some@mail.ru>
2021-10-08 22:30:19 +0300

    add models

--- backend/moretech/qraphs/models.py
+++ backend/moretech/qraphs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Operation(models.Model):
+    # кол-во элементов внутри оператора
+    NUMBER_OF_ELEMENTS = (
+        ('O', 'ONE'),
+        ('T', 'TWO'),
+        ('M', 'MANY'),
+    )
+    name = models.CharField(max_length=20)
+    number_of_elements = models.CharField(max_length=1, choices=NUMBER_OF_ELEMENTS)
+    json_data = models.TextField()
