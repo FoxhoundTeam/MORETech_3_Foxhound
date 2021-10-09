a18eec9a8030fabaac43942dc888ec3b650994da
kirill_semennikov <some@mail.ru>
2021-10-10 01:20:05 +0300

    add json proccesing

--- backend/moretech/graphs/urls.py
+++ backend/moretech/graphs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import data_table, feature, operations
+from .views import data_table, feature, operations, json_processing
 
 app_name = "graphs"
 # app_name will help us do a reverse look-up latter.
@@ -7,4 +7,5 @@ urlpatterns = [
     path('data_table', data_table.UserView.as_view()),
     path('feature', feature.UserView.as_view()),
     path('operation', operations.UserView.as_view()),
+    path('json', json_processing.UserView.as_view()),
 ]
