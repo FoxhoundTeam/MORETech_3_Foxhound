8cc4d9da7edd8a6c2c363830f4f40123dfbcc421
TurtleEgg <jake-take@yandex.ru>
2021-10-09 21:25:31 +0300

    WIP work started.

--- backend/moretech/moretech/settings.py
+++ backend/moretech/moretech/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oidc_provider',
     'rest_framework',
     'graphs'
 ]
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# OIDC settings
+LOGIN_URL = '/accounts/login/'
